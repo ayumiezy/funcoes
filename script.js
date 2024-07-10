f8b4d16fc8de0d379ab53a1fa5ee5c8ff4564de2
ayumi <105938742+ayumiezy@users.noreply.github.com>
2024-07-10 12:33:40 -0300

    af

--- script.js
+++ script.js
@@ -50,5 +50,12 @@ function mostraPergunta() {
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
 
 mostraPergunta();
